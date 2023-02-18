9cdeef0a5354ae20b2b0d7665808c78e1615352e
Li Qi <973129554@qq.com>
2023-02-18 11:20:15 +0800

    feat: Add common response

---
+++ blog-system-component/src/main/java/cc/lq/blog/system/util/BackendErrorStatus.java
@@ -0,0 +1,73 @@
+package cc.lq.blog.system.util;
+
+/**
+ * <p>
+ *     后端出错信息
+ * </p>
+ *
+ * @see <a href="https://github.com/alibaba/p3c/blob/master/Java%E5%BC%80%E5%8F%91%E6%89%8B%E5%86%8C(%E9%BB%84%E5%B1%B1%E7%89%88).pdf">Alibaba Java Develop Manual</a>
+ * @author Qi Li
+ * @since 2023-02-18
+ */
+public enum BackendErrorStatus {
+    OK(Series.SUCCESSFUL, "00000", "一切OK"),
+
+    CLIENT_ERROR(Series.CLIENT_ERROR,"A0001", "用户端错误"),
+    CLIENT_SIGN_UP_ERROR(Series.CLIENT_ERROR,"A0100", "用户注册错误"),
+
+    SERVER_ERROR(Series.SERVER_ERROR, "B0001", "系统执行出错"),
+    SERVER_TIMEOUT_ERROR(Series.SERVER_ERROR, "B0100", "系统执行超时"),
+
+    MIDDLEWARE_ERROR(Series.MIDDLEWARE_ERROR, "C0001", "调用第三方服务出错"),
+    MIDDLEWARE_DB_SERVICE_ERROR(Series.MIDDLEWARE_ERROR, "C0300", "数据库服务出错");
+
+    /**
+     * 后端出错分类
+     */
+    private final Series series;
+
+    /**
+     * 后端出错码
+     */
+    private final String errorCode;
+
+    /**
+     * 后端出错原因
+     */
+    private final String errorMessage;
+
+    BackendErrorStatus(Series series, String errorCode, String errorMessage) {
+        this.series = series;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public Series getSeries() {
+        return series;
+    }
+
+    public enum Series {
+        SUCCESSFUL("0"),
+        CLIENT_ERROR("A"),
+        SERVER_ERROR("B"),
+        MIDDLEWARE_ERROR("C");
+
+        private final String value;
+
+        Series(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
---
+++ blog-system-component/src/main/java/cc/lq/blog/system/util/CommonResponse.java
@@ -0,0 +1,149 @@
+package cc.lq.blog.system.util;
+
+
+import java.util.Collections;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+
+/**
+ * <p>
+ *     接口统一响应
+ * </p>
+ *
+ * @since 2023-02-18
+ * @author Qi Li
+ * @see <a href="https://alibaba.github.io/Alibaba-Java-Coding-Guidelines/">Alibaba Java Code Guidelines</a>
+ * @see <a href="https://github.com/alibaba/p3c/blob/master/Java%E5%BC%80%E5%8F%91%E6%89%8B%E5%86%8C(%E9%BB%84%E5%B1%B1%E7%89%88).pdf">Alibaba Java Develop Manual</a>
+ */
+public class CommonResponse<T> {
+    /**
+     * HTTP状态码
+     * @see org.springframework.http.HttpStatus
+     */
+    private Integer statusCode;
+
+    /**
+     * 后端出错码
+     */
+    private String errorCode;
+
+    /**
+     * 后端出错原因
+     */
+    private String errorMessage;
+
+    /**
+     * 用户提示信息
+     */
+    private String userMessage;
+
+    /**
+     * 响应数据
+     */
+    private List<T> data;
+
+    /**
+     * <p>
+     *     Success
+     * </p>
+     * @param data 数据
+     * @return 统一响应
+     */
+    public static <U> CommonResponse<U> success(List<U> data) {
+        return new CommonResponse<>(
+                HttpStatus.OK.value(),
+                BackendErrorStatus.OK.getErrorCode(),
+                BackendErrorStatus.OK.getErrorMessage(),
+                HttpStatus.OK.getReasonPhrase(),
+                data);
+    }
+
+    public static CommonResponse<Object> success() {
+        return new CommonResponse<>(
+                HttpStatus.OK.value(),
+                BackendErrorStatus.OK.getErrorCode(),
+                BackendErrorStatus.OK.getErrorMessage(),
+                HttpStatus.OK.getReasonPhrase(),
+                Collections.emptyList());
+    }
+
+    /**
+     * <p>
+     *     Fail
+     * </p>
+     *
+     * @param httpStatus HTTP状态信息
+     * @param backendErrorStatus 后端出错信息
+     * @return 统一响应
+     */
+    public static CommonResponse<Object> fail(HttpStatus httpStatus, BackendErrorStatus backendErrorStatus) {
+        return new CommonResponse<>(
+                httpStatus.value(),
+                backendErrorStatus.getErrorCode(),
+                backendErrorStatus.getErrorMessage(),
+                httpStatus.getReasonPhrase(),
+                Collections.emptyList());
+    }
+
+    private CommonResponse() {
+    }
+
+    private CommonResponse(Integer statusCode, String errorCode, String errorMessage, String userMessage, List<T> data) {
+        this.statusCode = statusCode;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.userMessage = userMessage;
+        this.data = data;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+    public void setUserMessage(String userMessage) {
+        this.userMessage = userMessage;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "CommonResponse{" +
+                "statusCode=" + statusCode +
+                ", errorCode='" + errorCode + '\'' +
+                ", errorMessage='" + errorMessage + '\'' +
+                ", userMessage='" + userMessage + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
